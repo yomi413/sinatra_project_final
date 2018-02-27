fd40d8604c4d9411c9528103c862c78c37eaf66e
github username <github email address>
2018-02-27 14:09:10 -0500

    Initialized ApplicationController

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do 
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'cars_collection'
+  end
+
+end
