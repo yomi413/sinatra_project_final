c85db9d630e1e4582c08467d2dcea85359ab9318
github username <github email address>
2018-02-27 23:59:01 -0500

    Modified User model to test validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   has_many :cars
   has_secure_password
-  validates :user_name, presence: true
-  validates :password, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
+  validate_presence_of :username, :password, :first_name, :last_name, :email
+  # validates :user_name, presence: true
+  # validates :password, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :email, presence: true
 end
