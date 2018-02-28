ba38a2d3baf82e7368ea7975d4de957d13b79158
github username <github email address>
2018-02-28 09:08:32 -0500

    Started laying out the structure for new entries

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,12 @@
 class CarsController < ApplicationController
 
   get '/cars' do 
+    @user = User.find_by(id: session[:user_id])
     erb :'/cars/cars'
   end 
+
+  get '/cars/new' do
+    erb :'/cars/new_car'
+  end
 
 end
