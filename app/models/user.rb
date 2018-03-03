2cd2e46a1ad5cf3fe2c079665852f945b1a882e3
github username <github email address>
2018-03-03 12:51:10 -0500

    Now adding validation for users email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ require './config/environment'
 class User < ActiveRecord::Base
   has_many :cars
   has_secure_password
-  validates_presence_of :first_name, :last_name, :username, :password
+  validates_presence_of :first_name, :last_name, :email, :username, :password
 end
