dd8960ba5eeb52158dd1e556d3b2360124593115
github username <github email address>
2018-03-26 22:07:40 -0400

    Added the #first_name method to User model; modified the cars.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   has_many :cars
   has_secure_password
   validates_presence_of :full_name, :username, :password
+
+  def first_name
+    self.full_name.split[0]
+  end
 end
