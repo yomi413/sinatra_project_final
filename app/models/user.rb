b58f2141ccdfe777f20c30dac104caa761efbb81
github username <github email address>
2018-03-26 22:58:11 -0400

    Modified the validations in the User model; modified the signup.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ require './config/environment'
 class User < ActiveRecord::Base
   has_many :cars
   has_secure_password
-  validates_presence_of :full_name, :username, :password
+  validates :full_name, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, uniqueness: true
 
   def first_name
     self.full_name.split[0]
